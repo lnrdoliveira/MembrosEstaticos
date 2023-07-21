3bc3baec7c5c3b054fc8155f54eab792d2530704
Leonardo <lnrdoliveira.dev@gmail.com>
2023-07-21 15:56:47 -0300

    Terceira solução

--- Calculadora.cs
+++ Calculadora.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 
 namespace file02{
     public class Calculadora{
-        public double Pi = 3.14;
-        public double Circunferencia(double r){
+        public static double Pi = 3.14;
+        public static double Circunferencia(double r){
             return 2.0*Pi*r;
         }
-        public double Volume(double r){
+        public static double Volume(double r){
             return 4.0/3.0*Pi*Math.Pow(r, 3.0);
         }
         
--- Program.cs
+++ Program.cs
@@ -5,15 +5,15 @@ namespace Course{
     class Program{
 
         static void Main(String[] args){
-            Calculadora calc = new Calculadora();
+            //Calculadora calc = new Calculadora();
 
             System.Console.Write("Entre com o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double circ = calc.Circunferencia(raio);
-            double volume = calc.Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
             System.Console.WriteLine("Circunferência: "+ circ.ToString("F2", CultureInfo.InvariantCulture));
             System.Console.WriteLine("Volume: "+ volume.ToString("F2", CultureInfo.InvariantCulture));
-            System.Console.WriteLine("Valor de PI: "+calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Valor de PI: "+Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
         }
     }
